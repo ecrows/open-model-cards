cd7cbf2ce8e3e1386e5c487f093b29516df1c14e
Evan Crothers <evan.of.crows@gmail.com>
2020-08-19 16:53:31 -0400

    Adding print button and improving print support (on Chrome)

--- src/card/Card.js
+++ src/card/Card.js
@@ -10,9 +10,12 @@ import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
+import IconButton from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListSubheader from '@material-ui/core/ListSubheader';
+import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
+import PrintIcon from '@material-ui/icons/Print';
 
 import ReactMarkdown from 'react-markdown'
 import { Link } from "react-router-dom";
@@ -22,21 +25,52 @@ import './Card.css';
 
 const drawerOffset = "12%";
 const drawerWidth = 280;
-const maxContentWidth = "64%";
-//const edgeWidth = drawerOffset + drawerWidth;
-
+const maxContentWidth = "82%";
 
 const useStyles = makeStyles((theme) => ({
   gridRoot: {
     flexGrow: 1,
   },
+  gridItem: {
+    breakInside: 'avoid',
+    breakBefore: 'recto',
+    display: 'block',
+    position: 'relative',
+  },
   root: {
     display: 'flex',
   },
+  menuButton: {
+    '@media print' : {
+      display: "none"
+    },
+    [theme.breakpoints.up('md')]: {
+      display: "none"
+    },
+    marginRight: theme.spacing(2),
+    width: "auto",
+    cursor: "pointer"
+  },
+  printButton: {
+    '@media print' : {
+      display: "none"
+    },
+    width: "auto",
+    cursor: "pointer"
+  },
+  appBarHeading: {
+    flexGrow: 1,
+  },
   appBar: {
+    '@media print' : {
+      boxShadow: 'none',
+      color: 'black',
+      marginLeft: 0,
+      width: "100%",
+      maxWidth: "100%",
+      marginTop: "1em"
+    },
     [theme.breakpoints.down('md')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: `calc(${drawerWidth}px)`,
       maxWidth: "100%",
     },
     [theme.breakpoints.down('sm')]: {
@@ -46,10 +80,12 @@ const useStyles = makeStyles((theme) => ({
     },
     left: 0,
     maxWidth: maxContentWidth,
-    width: `calc(100% - ${drawerOffset} - ${drawerWidth}px)`,
-    marginLeft: `calc(${drawerOffset} + ${drawerWidth}px)`,
+    width: "100%"
   },
   drawer: {
+    '@media print' : {
+      width: 0,
+    },
     [theme.breakpoints.down('sm')]: {
       width: 0,
     },
@@ -60,6 +96,9 @@ const useStyles = makeStyles((theme) => ({
     fontSize:'12px'
   },
   drawerRoot: {
+    '@media print' : {
+      marginLeft: 0,
+    },
     [theme.breakpoints.down('md')]: {
       marginLeft: 0
     },
@@ -69,6 +108,9 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: drawerOffset
   },
   drawerPaper: {
+    '@media print' : {
+      width: 0,
+    },
     [theme.breakpoints.down('sm')]: {
       width: 0,
     },
@@ -76,14 +118,14 @@ const useStyles = makeStyles((theme) => ({
   },
   paperAnchorLeft: {
     left: "auto",
-    right: "auto"
+    right: "auto",
+    borderRight: "1px solid rgba(0, 0, 0, 0.12)",
+    borderLeft: "1px solid rgba(0, 0, 0, 0.12)"
   },
   // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
   content: {
     display: "flex",
-    flexDirection: "column",
-    flexGrow: 1,
     //backgroundColor: theme.palette.background.default,
     backgroundColor: "white",
     [theme.breakpoints.down('sm')]: {
@@ -103,16 +145,6 @@ function Card(props) {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar
-        position="absolute" // was fixed
-        className={classes.appBar}
-        style={{backgroundColor: data.color}}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            {data.title}
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <Drawer
         className={classes.drawer}
         variant="permanent"
@@ -158,33 +190,51 @@ function Card(props) {
             </List>
           </div>
       </Drawer>
-        <main className={classes.content}>
-        <div className={classes.toolbar} />
-          <div className="card-page">
-          <div className="card-content">
-            <div className={classes.gridRoot}>
-              <Grid container spacing={5}>
-                <Grid item xs={12}>
-                  <div className="point-body">
-                    <ReactMarkdown source={data.page_heading}/>
-                  </div>
-                  </Grid>
-                    {
-                      data.sections.map((b, i) => 
-                      <Grid item xs={12} sm={6} key={i + b.heading}>
-                      <div className="point-heading" id={b.heading}>{b.heading ? b.heading : "\u00a0"}</div>
-                      <div className="point-body">
-                        <ReactMarkdown source={b.content}/>
-                      </div>
+      <div className="rightPane">
+        <AppBar
+          position="relative" // was fixed
+          className={classes.appBar}
+          style={{backgroundColor: data.color}}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap className={classes.appBarHeading}>
+              {data.title}
+            </Typography>
+            <IconButton className={classes.printButton} aria-label="print page" color="inherit">
+                <PrintIcon onClick={window.print} />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+          <main className={classes.content}>
+          <div className={classes.toolbar} />
+            <div className="card-page">
+            <div className="card-content">
+              <div className={classes.gridRoot}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12} classes={{item: classes.gridItem}}>
+                    <div className="point-body">
+                      <ReactMarkdown source={data.page_heading}/>
+                    </div>
                     </Grid>
-                    )
-                  }
-                </Grid>
+                      {
+                        data.sections.map((b, i) => 
+                        <Grid item xs={12} sm={6} key={i + b.heading} classes={{item: classes.gridItem}}>
+                        <div className="point-heading" id={b.heading}>{b.heading ? b.heading : "\u00a0"}</div>
+                        <div className="point-body">
+                          <ReactMarkdown source={b.content}/>
+                        </div>
+                      </Grid>
+                      )
+                    }
+                  </Grid>
+                </div>
               </div>
+              <Footer/>
             </div>
-            <Footer/>
-          </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 }
